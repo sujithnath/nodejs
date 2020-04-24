f7627748acd1e26d3e273484407b661ca67db0c3
sujithnath <sujithgnth@gmail.com>
2020-04-24 16:11:51 +0530

    new file chagnes

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,23 @@
-const geocode = require('./utils/geocode');
-const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const address = process.argv[2];
+const address = process.argv[2]
 
 if (!address) {
-  console.log('Please provide an address');
+    console.log('Please provide an address')
 } else {
-  geocode(address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return console.log(error);
-    }
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
 
-    forecast(latitude, longitude, (error, forecastData) => {
-      if (error) {
-        return console.log(error);
-      }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
 
-      console.log(location);
-      console.log(forecastData);
-    });
-  });
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
 }
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -22,14 +23,14 @@ app.use(express.static(publicDirectoryPath));
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather',
-    name: 'Sujith Gopinath',
+    name: 'Sujith',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Sujith Gopinath',
+    name: 'Sujith',
   });
 });
 
@@ -37,7 +38,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     helpText: 'This is some helpful text.',
     title: 'Help',
-    name: 'Sujith Gopinath',
+    name: 'Sujith',
   });
 });
 
@@ -86,7 +87,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Sujith Gopinath',
+    name: 'Sujith',
     errorMessage: 'Help article not found.',
   });
 });
@@ -94,11 +95,11 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
   res.render('404', {
     title: '404',
-    name: 'Sujith Gopinath',
+    name: 'Sujith',
     errorMessage: 'Page not found.',
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port);
 });
