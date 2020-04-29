b458f5bc74d9be8ab2016e8c0ace405517c18ee1
sujithnath <sujithgnth@gmail.com>
2020-04-29 08:52:07 +0530

    new changes

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,53 +1,95 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const Filter = require('bad-words')
-const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require('./utils/messages');
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require('./utils/users');
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 io.on('connection', (socket) => {
-    console.log('New WebSocket connection')
+  console.log('New WebSocket connection');
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+  socket.on('join', (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    if (error) {
+      return callback(error);
+    }
 
-        // socket.emit, io.emit, socket.broadcast.emit
-        // io.to.emit, socket.broadcast.to.emit
-    })
+    socket.join(user.room);
 
-    socket.on('sendMessage', (message, callback) => {
-        const filter = new Filter()
+    socket.emit('message', generateMessage('Admin', 'Welcome!'));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        generateMessage('Admin', `${user.username} has joined!`)
+      );
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
-        if (filter.isProfane(message)) {
-            return callback('Profanity is not allowed!')
-        }
+    callback();
+  });
 
-        io.to('Center City').emit('message', generateMessage(message))
-        callback()
-    })
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    const filter = new Filter();
 
-    socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-        callback()
-    })
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
 
-    socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
-    })
-})
+    io.to(user.room).emit('message', generateMessage(user.username, message));
+    callback();
+  });
+
+  socket.on('sendLocation', (coords, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
+      'locationMessage',
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+      )
+    );
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        generateMessage('Admin', `${user.username} has left!`)
+      );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`)
-})
+  console.log(`Server is up on port ${port}!`);
+});
